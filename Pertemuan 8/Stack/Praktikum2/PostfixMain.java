519c791a0536e5db421963774da431799d658ad1
qorinapramewari <qorinaanindhita07@gmail.com>
2020-03-26 10:10:30 +0700

    tambah folder

---
+++ Pertemuan 8/Stack/Praktikum2/PostfixMain.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Praktikum2;
+import java.util.Scanner;
+/**
+ *
+ * @author Prameswari
+ */
+public class PostfixMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String P, Q;
+        System.out.println("Masukkan Ekspresi Matematika : ");
+        Q = sc.nextLine();
+        Q = Q.trim();
+        Q = Q + ")";
+        
+        int total = Q.length();
+        Postfix post = new Postfix(total);
+        P = post.konversi(Q);
+        System.out.println("Postfix : " + P);
+    }
+    
+}
